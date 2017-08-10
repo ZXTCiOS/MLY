5065a5fda228c53bb809fac632f664baccd22363
apple <xudogn@qq.com>
2017-08-10 13:53:02 +0800

    qqkey

--- 蒙爱你/Vendor/ZTVendorManager/ZTVendorManagerConfig.h
+++ 蒙爱你/Vendor/ZTVendorManager/ZTVendorManagerConfig.h
@@ -10,7 +10,7 @@
 #define ZTShare_PayConfig_h
 
 //UMeng https://i.umeng.com
-static NSString * const kUMAppKey = @"59126acacae7e72a4b001a1c";
+static NSString * const kUMAppKey = @"598bdcfb1c5dd048bf001514";
 
 //Vendor: Sina http://open.weibo.com
 static NSString * const kSinaAppKey = @"427975783";
@@ -18,8 +18,8 @@ static NSString * const kSinaRedirectURL =  @"http://sns.whalecloud.com/sina2/ca
 static NSString * const kSinaAppSecret = @"975144dcda0487b4e36a48f85d98c8e0";
 
 //Vendor:Tencnet QQ http://open.qq.com
-static NSString * const kTencentQQAppID = @"1105151880"; //0x41DF4788
-static NSString * const kTencentQQAppKey = @"huLVBLX3q86ZsqN5";
+static NSString * const kTencentQQAppID = @"1106264053"; //0x41DF4788
+static NSString * const kTencentQQAppKey = @"pUm1N8h2PSxLvuUn";
 
 //Vendor:WeChat  https://open.weixin.qq.com
 static NSString * const kWeChatAppID = @"wx9d022487205c4ada";
